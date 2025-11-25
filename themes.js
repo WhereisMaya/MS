48e69187e38943d5b7da8c13eee74e21070bbe8b
Jannerap <Jannerap@protonmail.com>
2025-11-25 15:40:42 +0000

    dfv

--- themes.js
+++ themes.js
@@ -6627,6 +6627,42 @@ const themePresets = {
         ],
         bg: "images/default25.png"
       },
+      "transplus": {
+        name: "GIC - Transplus",
+        ideas: [
+          {
+            "title": "📝 Example",
+            "description": "Click to edit",
+            "x": 400,
+            "y": 300,
+            "vx": 0,
+            "vy": 0,
+            "color": "hsl(200, 100%, 70%)",
+            "textColor": "white",
+            "radius": 80,
+            "font": "Arial",
+            "glow": true,
+            "fontSize": 16,
+            "rotation": 0
+          },
+          {
+            "title": "💡 Template",
+            "description": "Start here",
+            "x": 600,
+            "y": 400,
+            "vx": 1.5,
+            "vy": -1.2,
+            "color": "hsl(120, 100%, 70%)",
+            "textColor": "white",
+            "radius": 70,
+            "font": "Arial",
+            "glow": true,
+            "fontSize": 14,
+            "rotation": 0
+          }
+        ],
+        bg: "images/default25.png"
+      },
       "gmc": {
         name: "GMC",
         ideas: [
